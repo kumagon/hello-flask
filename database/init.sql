aee956c1373aa30bec3c202da6d50b1e23c55f98
kumagon <kumagon0619@gmail.com>
2022-06-03 13:30:56 +0800

    db

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,12 @@
-CREATE TABLE item {
-...
-}
+CREATE TABLE item (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(50),
+  description VARCHAR(255),
+  userId INT(11),
+  createdAt DATETIME,
+  updatedAt DATETIME,
+  PRIMARY KEY (id)
+);
 
-INSERT INTO item ...
+INSERT INTO item (title, description, userId, createdAt, updatedAt) VALUE ('test', 'test', 1, NOW(), NOW());
+
